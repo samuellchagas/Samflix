f5564890cf9e77c608c478314de96a932d8af92a
samuellchagas <samuell.tomaz@compasso.com.br>
2022-01-20 20:37:48 -0300

    Implement librarys and init injection dependence (Koin)

---
+++ app/src/main/java/com/compass/poc/samflix/di/MyApp.kt
@@ -0,0 +1,17 @@
+package com.compass.poc.samflix.di
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class MyApp:Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@MyApp)
+            modules(myModules)
+        }
+    }
+}
---
+++ app/src/main/java/com/compass/poc/samflix/di/myModules.kt
@@ -0,0 +1,9 @@
+package com.compass.poc.samflix.di
+
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+
+val myModules = module {
+
+
+}
